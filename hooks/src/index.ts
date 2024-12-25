287777164e984e55dc762ad55d5ee6b73f6c1ec8
Bishal Baira <bishalbaira432@gmail.com>
2024-12-25 15:58:58 +0530

    added server routes

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,16 +1,37 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
+
+const client = new PrismaClient();
 
 const app = express();
+app.use(express.json());
 
 // https://hooks.integrix.com/hooks/catch/:userId/:zapId
-app.post("/hooks/catch/:userId/:zapId", (req, res) => {
-    const userId = req.params.userId;
-    const zapId = req.params.zapId;
+app.post("/hooks/catch/:userId/:zapId", async (req, res) => {
+  const userId = req.params.userId;
+  const zapId = req.params.zapId;
+  const body = req.body;
 
-    // store in db
-    // push it on a queue
-})
+  // store in db
+  await client.$transaction(async (tx) => {
+    const run = await tx.zapRun.create({
+      data: {
+        zapId: zapId,
+        metadata: body,
+      },
+    });
+
+    await tx.zapRunOutbox.create({
+      data: {
+        zapRunId: run.id,
+      },
+    });
+  });
+  res.json({
+    message: "Webhook received",
+  });
+});
 
 app.listen(3000, () => {
-    console.log("Server started on port 3000");
+  console.log("Server started on port 3000");
 });
